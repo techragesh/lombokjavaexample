d917f61912a2fceb9c169256dc4b330c70e6ff8b
ragesh <ragesh@Elizabeths-MacBook-Pro.local>
2017-12-26 20:44:59 +0100

    Lombok Java example

---
+++ src/main/java/com/techjava/App.java
@@ -0,0 +1,16 @@
+package com.techjava;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.setUserId(1);
+        user.setUsername("Ajay");
+        System.out.println(user);
+
+    }
+}
---
+++ src/main/java/com/techjava/User.java
@@ -0,0 +1,35 @@
+package com.techjava;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int userId;
+
+    private String username;
+
+//    public int getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(int userId) {
+//        this.userId = userId;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "userId=" + userId +
+//                ", username='" + username + '\'' +
+//                '}';
+//    }
+}
